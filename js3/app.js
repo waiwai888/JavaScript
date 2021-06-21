9a690d970bf83c6331b50de1a5e0a76bb6a470d1
waiwai888 <yuisato9688@gmail.com>
2021-06-21 15:43:03 +0900

    [finish]js3確認問題

--- js3/app.js
+++ js3/app.js
@@ -1,10 +1,18 @@
 var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+    alert("グー・チョキ・パーのいずれかを入力してください");
+    user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+    }
 
 var js_hand = getJShand();
 
 var judge = winLose(user_hand, js_hand);
 
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if (user_hand != null){
+    alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+  } else {
+    alert("またチャレンジしてね")
+  }
 
 function getJShand(){
     var js_hand_num = Math.floor( Math.random() * 3 );
@@ -18,7 +26,7 @@ function getJShand(){
     }
   
     return js_hand;
-  }
+    }
 
   function winLose(user, js){
     var winLoseStr;

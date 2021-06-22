5ffc50d195c2524263601ada823772a4c745b2a2
waiwai888 <yuisato9688@gmail.com>
2021-06-22 15:47:55 +0900

    [finish]js12

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,7 +1,14 @@
-$(function() {
-    $('.menu-trigger').on('click', function(event) {
-      $(this).toggleClass('active');
-      $('#sp-menu').fadeToggle();
-      event.preventDefault();
+const API_KEY = "3fea7b9060a0ce293df34d3f7a8a76e9";
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "3fea7b9060a0ce293df34d3f7a8a76e9",
+      dataType : 'jsonp',
+      }).done(function (data){
+        //通信成功
+      }).fail(function (data) {
+        //通信失敗
     });
+    })
   });
+});
